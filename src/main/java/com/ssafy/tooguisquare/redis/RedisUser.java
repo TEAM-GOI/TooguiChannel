c1191ac0a0a0b32ae002b1932d11fd3dfdc72942
wonjunJ <museoul@naver.com>
2025-07-25 14:50:05 +0900

    [Fix] 유저 정보 캐싱 위한 RedisUser 수정

--- src/main/java/com/ssafy/tooguisquare/redis/RedisUser.java
+++ src/main/java/com/ssafy/tooguisquare/redis/RedisUser.java
@@ -15,4 +15,11 @@ public class RedisUser implements Serializable {
 
     //false == 대기중  // true == 게임중
     private boolean status;
+
+    // 방 입/퇴장시 필요한 유저 정보를 캐싱하여 저장하기 위해
+    private String nickName;
+
+    private Long exp;
+
+    private int imageId;
 }
